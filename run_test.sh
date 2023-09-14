084ee5b29d75b4d5042bd8307eb7e7a2c976193c
segfault_magnet <ahmed.sagdati.ets@gmail.com>
2023-09-14 12:32:46 +0200

    hey

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-#
 num_files="$(find fuel-core | wc -l)"
 
-if [[ $num_files == "0" ]]; then
-	echo "Be sure to checkout the $(fuel-core) submodule: git submodule update --init --recursive"
+if [[ $num_files == "1" ]]; then
+	echo "Be sure to checkout the 'fuel-core' submodule: git submodule update --init --recursive"
 	exit 1
 fi
 
