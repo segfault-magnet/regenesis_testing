632aed00e27edf8600ef5fa88b6245e0ba7cf770
segfault_magnet <ahmed.sagdati.ets@gmail.com>
2023-09-14 12:30:20 +0200

    added submodule

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+#
+num_files="$(find fuel-core | wc -l)"
 
+if [[ $num_files == "0" ]]; then
+	echo "Be sure to checkout the $(fuel-core) submodule: git submodule update --init --recursive"
+	exit 1
+fi
+
+echo "Building fuel-core"
+(cd fuel-core && cargo xtask build)
+echo "Running tests"
 PATH="./fuel-core/target/debug/:$PATH"
 exec cargo test
